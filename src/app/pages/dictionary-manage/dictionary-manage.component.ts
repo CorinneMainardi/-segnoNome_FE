aa08b1043ff3ad1f622b56c5ea754ee714293110
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2025-02-08 01:11:00 +0100

    dictionary management

--- src/app/pages/dictionary-manage/dictionary-manage.component.ts
+++ src/app/pages/dictionary-manage/dictionary-manage.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
+import { iDictionary } from '../../interfaces/i-dictionary';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { iVideoClass } from '../../interfaces/i-video-class';
+import { VideoclassesService } from '../../services/videoclasses.service';
+import { DictionaryService } from '../../services/dictionary.service';
 
 @Component({
   selector: 'app-dictionary-manage',
   templateUrl: './dictionary-manage.component.html',
-  styleUrl: './dictionary-manage.component.scss'
+  styleUrl: './dictionary-manage.component.scss',
 })
 export class DictionaryManageComponent {
+  dictionary: iDictionary[] = [];
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
+  editingVideo: iDictionary | null = null; // Variabile per gestire il video in modifica
+  validateForm!: FormGroup;
 
+  constructor(
+    private dictionarySvc: DictionaryService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.dictionarySvc.getAllDictionaryVideos().subscribe({
+      next: (dictionary) => {
+        this.dictionary = dictionary;
+      },
+      error: (err) => {
+        console.error('Errore durante il recupero dei video', err);
+        this.errorMessage = 'Errore durante il recupero dei video';
+      },
+    });
+
+    // Inizializzare il form
+    this.validateForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      dictionaryUrl: ['', [Validators.required]],
+    });
+  }
+
+  deleteVideo(id: number | undefined): void {
+    if (id !== undefined) {
+      this.dictionarySvc.deleteDictionaryVideo(id).subscribe({
+        next: () => {
+          this.dictionary = this.dictionary.filter((vc) => vc.id !== id);
+        },
+        error: (err) => {
+          console.error("❌ Errore durante l'eliminazione del video", err);
+          this.errorMessage = "Errore durante l'eliminazione del video";
+        },
+      });
+    } else {
+      console.error('❌ Video id is undefined!');
+    }
+  }
+
+  editVideo(video: iDictionary): void {
+    this.editingVideo = video;
+    this.validateForm.patchValue(video); // Precompilo il form con i dati del video
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid && this.editingVideo) {
+      const updatedDictionary = this.validateForm.value;
+      //devo controllare che l'id non sia undefined
+      if (this.editingVideo.id !== undefined) {
+        this.dictionarySvc
+          .updateDictionaryVideo(this.editingVideo.id, updatedDictionary)
+          .subscribe({
+            next: () => {
+              this.successMessage = '✅ Video aggiornato con successo!';
+              this.errorMessage = null; // Resetta eventuali errori
+              this.editingVideo = null; // Reset del video in modifica
+              this.validateForm.reset(); // Reset del form
+            },
+            error: (err) => {
+              this.errorMessage = "❌ Errore durante l'aggiornamento del video";
+              this.successMessage = null;
+            },
+          });
+      } else {
+        console.error('❌ ID del video non valido');
+      }
+    } else {
+      console.log('❌ Il form non è valido');
+    }
+  }
+  confirmDelete(video: iDictionary) {
+    if (confirm(`Sei sicuro di voler eliminare il video "${video.title}"?`)) {
+      this.deleteVideo(video.id);
+    }
+  }
+  // Metodo per resettare il form
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    this.editingVideo = null;
+  }
 }
--- src/app/pages/dictionary-manage/dictionary-manage.module.ts
+++ src/app/pages/dictionary-manage/dictionary-manage.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { DictionaryManageRoutingModule } from './dictionary-manage-routing.module';
 import { DictionaryManageComponent } from './dictionary-manage.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @NgModule({
-  declarations: [
-    DictionaryManageComponent
-  ],
+  declarations: [DictionaryManageComponent],
   imports: [
     CommonModule,
-    DictionaryManageRoutingModule
-  ]
+    DictionaryManageRoutingModule,
+    NzIconModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzSelectModule,
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, provideAnimationsAsync()],
 })
-export class DictionaryManageModule { }
+export class DictionaryManageModule {}
